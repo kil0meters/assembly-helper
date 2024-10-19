4aa098e0f3406ea9fe848cf9a917c62bf1d83771
kilometers <kilo@meters.sh>
2024-10-18 22:55:54 -0700

    client side routing

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-emcc --no-entry -s EXPORTED_FUNCTIONS='["_init"]' -s EXPORTED_RUNTIME_METHODS='["cwrap"]' -o static/main.js src/main.c
+emcc \
+    --no-entry \
+    -s EXPORTED_FUNCTIONS='["_init", "_render_on_route"]' \
+    -sDEFAULT_LIBRARY_FUNCS_TO_INCLUDE='$stringToNewUTF8' \
+    -o static/main.js src/main.c
+# -s EXPORTED_RUNTIME_METHODS='["cwrap", "render_on_route"]'
--- src/main.c
+++ src/main.c
@@ -5,26 +5,45 @@
 #include "components/lesson_overview.h"
 #include "components/lesson_list.h"
 
+char render_buffer[HTML_BUFFER_SIZE];
+
 EM_JS(void, populate_selector_with_html, (const char *selector, const char *innerHTML), {
     let element = document.querySelector(UTF8ToString(selector));
     element.innerHTML = UTF8ToString(innerHTML);
 });
 
+
+EMSCRIPTEN_KEEPALIVE
+void render_on_route() {
+    char *route = (char*)EM_ASM_PTR({
+      return stringToNewUTF8(window.location.hash.substr(1));
+    });
+
+    jlog(route);
+
+    if (strncmp(route, "/", 2) == 0 || strncmp(route, "", 1) == 0) {
+        jlog("route 2");
+        LessonListItem items[3] = {
+            { .title = "Loop Unrolling", .slug = "#/lessons/unrolling" },
+            { .title = "Pipelining", .slug = "#/lessons/pipelining" },
+            { .title = "Baz", .slug = "#/lessons/baz" },
+        };
+
+        lesson_list_template(render_buffer, sizeof(items) / sizeof(LessonListItem), items);
+    } else if (strncmp(route, "/lessons/unrolling", sizeof("/lessons/unrolling")) == 0) {
+        jlog("route 2");
+        lesson_overview_template(render_buffer, "Loop Unrolling");
+    } else {
+        snprintf(render_buffer, HTML_BUFFER_SIZE, "<h1>404</h1>");
+    }
+
+    populate_selector_with_html("#content", render_buffer);
+    free(route);
+}
+
 // Initialize function
 EMSCRIPTEN_KEEPALIVE void init() {
-    char buffer[HTML_BUFFER_SIZE] = "wow";
-    // lesson_overview_template(buffer, "this is a title");
-    // populate_selector_with_html("#lesson-content", buffer);
-
-    LessonListItem items[3] = {
-        { .title = "Foo", .slug = "/foo" },
-        { .title = "Bar", .slug = "/bar" },
-        { .title = "Baz", .slug = "/baz" },
-    };
-
-    lesson_list_template(buffer, 3, items);
-    jlog(buffer);
-    populate_selector_with_html("#content", buffer);
-
-    emscripten_run_script("console.log('Initialized')");
+    emscripten_run_script("window.addEventListener('hashchange', () => Module._render_on_route())");
+    emscripten_run_script("window.addEventListener('load', () => Module._render_on_route())");
+    emscripten_run_script("Module._render_on_route()");
 }
